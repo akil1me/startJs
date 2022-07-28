eb69fb2c17d97f30eac55b5ff43cf94e607a12c0
akil1me <akipax123@gmail.com>
2022-07-28 12:13:25 +0500

    First commit and start to JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var intro = alert("Assalomu aleykum");
+console.log("F.I.SH: " + prompt("F.I.SH"));
+console.log("Yoshi: " + prompt("Yoshingiz nechida"));
+console.log("Qayerdanligi: " + prompt("Qayerdansiz"));
+console.log("Qayerda ta'lim olish " + prompt("Qayerda ta'lim olasiz, yoki ishlaysiz"));
+console.log("Oilaviy muhiti: " + prompt("Oilaviy muhit"));
+console.log("Tel raqami: " + prompt("Tel raqamingiz"));
+console.log("Email: " + prompt("Elektron pochtangiz(email,gmail,mail,yandex)"));
+console.log("Turar joy: " + prompt("Turar joyiz"));
+console.log("Poshta indexsi: " + prompt("Pochta indeksi"));
